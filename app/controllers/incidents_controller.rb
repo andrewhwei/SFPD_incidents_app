a8ebb8f57edc631181ad98cc498ec854f72065d6
Andrew Wei <andrew.wei92@yahoo.com>
2016-05-09 18:51:54 -0700

    initial commit

---
+++ app/controllers/incidents_controller.rb
@@ -0,0 +1,7 @@
+class IncidentsController < ApplicationController
+
+  def index
+    @incidents = Unirest.get("https://data.sfgov.org/resource/cuks-n6tp.json").body
+  end
+
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails.application.config.session_store :cookie_store, key: '_SFPD_incidents_app_session'
